5fa7bb365a7b4243d92cfdc6af88aa97fe840620
mari3720 <mari3720@362callisto>
2023-02-17 09:29:24 -0700

    initial files

---
+++ include/TreeQuery.h
@@ -0,0 +1,15 @@
+/*
+ * TreeQuery.h
+ *
+ *  Created on: Feb. 17, 2023
+ *      Author: Isaac Martens, Brett Wells
+ */
+
+#ifndef INCLUDE_TREEQUERY_H_
+#define INCLUDE_TREEQUERY_H_
+
+
+
+
+
+#endif /* INCLUDE_TREEQUERY_H_ */
---
+++ src/TreeQuery.cpp
@@ -0,0 +1,10 @@
+/*
+ * TreeQuery.cpp
+ *
+ *  Created on: Feb. 17, 2023
+ *      Author: Isaac Martens, Brett Wells
+ */
+
+
+
+
